3cd56309f9029ffe058d9a8cda29dc6d506175b0
Royhan <royhankeren12@gmail.com>
2023-12-27 15:57:14 +0700

    fist screen

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -10,90 +10,120 @@ class FirstScreen extends StatelessWidget {
     final TextEditingController txtName = TextEditingController(text: "");
     final TextEditingController txtPalindrome = TextEditingController(text: "");
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/ic_photo.png",
-              width: 116,
-              height: 116,
-              fit: BoxFit.cover,
-              color: Colors.grey,
-            ),
-            Container(
-              width: 310,
-              height: 47,
-              margin: const EdgeInsets.only(top: 51, bottom: 15),
-              child: TextFormField(
-                controller: txtName,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+      body: Container(
+        decoration: const BoxDecoration(
+          // Apply the gradient to the Container's decoration
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            colors: [
+              Color(0xFF325F8B),
+              Color(0xFF336C8D),
+              Color(0xFF5993A4),
+              Color(0xFF839EA9),
+            ],
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 116,
+                height: 116,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: const Color.fromARGB(255, 235, 235, 235)),
+                child: const Icon(
+                  Icons.person_add_alt_1,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+              Container(
+                width: 310,
+                height: 47,
+                margin: const EdgeInsets.only(top: 51, bottom: 15),
+                child: TextFormField(
+                  controller: txtName,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Name",
+                    hintStyle: GoogleFonts.poppins(
+                      color: const Color.fromARGB(255, 206, 205, 205),
+                    ),
+                    contentPadding: const EdgeInsets.only(left: 20),
                   ),
-                  hintText: "Name",
-                  hintStyle: GoogleFonts.poppins(color: Colors.grey),
                 ),
               ),
-            ),
-            Container(
-              width: 310,
-              height: 47,
-              margin: const EdgeInsets.only(bottom: 45),
-              child: TextFormField(
-                controller: txtPalindrome,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+              Container(
+                width: 310,
+                height: 47,
+                margin: const EdgeInsets.only(bottom: 45),
+                child: TextFormField(
+                  controller: txtPalindrome,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Palindrome",
+                    hintStyle: GoogleFonts.poppins(
+                      color: const Color.fromARGB(255, 206, 205, 205),
+                    ),
+                    contentPadding: const EdgeInsets.only(left: 20),
                   ),
-                  hintText: "Palindrome",
-                  hintStyle: GoogleFonts.poppins(color: Colors.grey),
                 ),
               ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(310, 41),
-                backgroundColor: const Color(0xff2B637B),
-                foregroundColor: Colors.lightBlue,
-                shadowColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {},
-              child: Text(
-                "CHECK",
-                style: GoogleFonts.poppins(color: Colors.white),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(310, 41),
-                backgroundColor: const Color(0xff2B637B),
-                foregroundColor: Colors.lightBlue,
-                shadowColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SecondScreen(),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(310, 41),
+                  backgroundColor: const Color.fromARGB(255, 34, 78, 97),
+                  foregroundColor: Colors.lightBlue,
+                  shadowColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                );
-              },
-              child: Text(
-                "NEXT",
-                style: GoogleFonts.poppins(color: Colors.white),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "CHECK",
+                  style: GoogleFonts.poppins(color: Colors.white),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(310, 41),
+                  backgroundColor: const Color.fromARGB(255, 34, 78, 97),
+                  foregroundColor: Colors.lightBlue,
+                  shadowColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SecondScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "NEXT",
+                  style: GoogleFonts.poppins(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
